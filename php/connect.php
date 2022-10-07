6868dccce4c41e904b71219c2d2e70969ffbeee0
Tim <timlabro@gmail.com>
2022-10-07 22:34:09 +0800

    Php enter data to the db (Error resolve)

--- php/connect.php
+++ php/connect.php
@@ -1,13 +1,14 @@
 <?php
     $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
+    $dBusername = "root";
+    $dBpassword = "";
     $dbName = "activity2";
 
     // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbName);
+    $conn = mysqli_connect($servername, $dBusername, $dBpassword, $dbName);
 
     // Check Connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
+    
